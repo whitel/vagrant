72877d7f1abac81711b2e6d8829d7ace6d430b72
langdon <langdon@fedoraproject.org>
2018-06-14 15:37:03 -0400

    tries to add a new cap for fedora for nfs but i don't remember why

--- plugins/guests/fedora/plugin.rb
+++ plugins/guests/fedora/plugin.rb
@@ -15,6 +15,16 @@ module VagrantPlugins
         require_relative "cap/flavor"
         Cap::Flavor
       end
+
+      guest_capability("fedora", "flavor") do
+        require_relative "cap/flavor"
+        Cap::Flavor
+      end
+
+      guest_capability("fedora", "nfs_client_install") do
+        require_relative "cap/nfs_client"
+        Cap::NFSClient
+      end
     end
   end
 end
